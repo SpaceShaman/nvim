4ae2767f57a0f1c0047d9ae881a426b0173b4f54
SpaceShaman <spaceshaman@tuta.io>
2025-06-16 14:13:38 +0200

    🔧 (init.lua): configure LSP for Python with Ruff and Pyright
    Add Ruff and Pyright configurations to enhance Python development.
    Disable Pyright's import organizer and analysis to rely on Ruff for
    linting and organizing imports. This change aims to streamline the
    development process by using Ruff's capabilities.

--- init.lua
+++ init.lua
@@ -631,6 +631,21 @@ require('lazy').setup({
             },
           },
         },
+        ruff = {},
+        pyright = {
+          settings = {
+            pyright = {
+              -- Using Ruff's import organizer
+              disableOrganizeImports = true,
+            },
+            python = {
+              analysis = {
+                -- Ignore all files for analysis to exclusively use Ruff for linting
+                ignore = { '*' },
+              },
+            },
+          },
+        },
       }
 
       -- Ensure the servers and tools above are installed
