b086fd8b8301561765db784901a42c62aa0bd011
SpaceShaman <spaceshaman@tuta.io>
2025-06-30 15:45:56 +0200

    ♻️ (indent_rainbowline.lua): refactor color scheme for better readability ✨ (indent_rainbowline.lua): add support for whitespace highlighting ✨ (indent_rainbowline.lua): introduce dynamic highlight naming for colors

--- lua/plugins/indent_rainbowline.lua
+++ lua/plugins/indent_rainbowline.lua
@@ -4,14 +4,20 @@ return {
   config = function()
     local is_dark = vim.o.background == 'dark'
 
-    local colors = is_dark and { '#662121', '#767621', '#216631', '#325a5e', '#324b7b', '#562155' } -- ayu-dark
-      or { '#e57373', '#dce775', '#81c784', '#4dd0e1', '#64b5f6', '#ba68c8' } -- ayu-light
+    local colors = is_dark and { '#2e1a1a', '#2f2f1a', '#1a2e1a', '#1a2a2e', '#1a1f2f', '#2a1a2f' }
+      or { '#ffeaea', '#ffffe0', '#e8f5e9', '#e0f7fa', '#e3f2fd', '#f3e5f5' }
 
     local hooks = require 'ibl.hooks'
+
+    local highlight = {}
+    for i in ipairs(colors) do
+      table.insert(highlight, 'indent_color_' .. i)
+    end
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
       for i, color in ipairs(colors) do
-        vim.api.nvim_set_hl(0, 'IndentBlanklineIndent' .. i, {
-          fg = color,
+        local color_name = 'indent_color_' .. i
+        vim.api.nvim_set_hl(0, color_name, {
+          bg = color,
           nocombine = true,
         })
       end
@@ -19,14 +25,15 @@ return {
 
     require('ibl').setup {
       indent = {
-        highlight = {
-          'IndentBlanklineIndent1',
-          'IndentBlanklineIndent2',
-          'IndentBlanklineIndent3',
-          'IndentBlanklineIndent4',
-          'IndentBlanklineIndent5',
-          'IndentBlanklineIndent6',
-        },
+        char = '',
+        highlight = highlight,
+      },
+      whitespace = {
+        highlight = highlight,
+        remove_blankline_trail = false,
+      },
+      scope = {
+        enabled = false,
       },
     }
   end,
