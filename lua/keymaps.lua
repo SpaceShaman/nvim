e637e2983762e51238cb845297d364f1fd874517
SpaceShaman <spaceshaman@tuta.io>
2025-07-03 16:32:50 +0200

    ✨ (keymaps.lua): add buffer swapping functionality with neighboring windows

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -17,10 +17,41 @@ return function()
   map('n', '<A-l>', '<C-w><Up>', { desc = 'Go to the upper window', noremap = true })
   map('n', '<A-;>', '<C-w><Right>', { desc = 'Go to the right window', noremap = true })
 
-  map('n', '<Leader>j', '<Cmd>WinShift left<CR>', { desc = 'Move window to far lower', noremap = true })
-  map('n', '<Leader>k', '<Cmd>WinShift down<CR>', { desc = 'Move window to far lower', noremap = true })
-  map('n', '<Leader>l', '<Cmd>WinShift up<CR>', { desc = 'Move window to far upper', noremap = true })
-  map('n', '<Leader>;', '<Cmd>WinShift right<CR>', { desc = 'Move window right', noremap = true })
+  local function swap_buffer_with_neighbor(direction)
+    local cur_win = vim.api.nvim_get_current_win()
+    local cur_buf = vim.api.nvim_win_get_buf(cur_win)
+
+    -- Move in the given direction (only to get the target window)
+    vim.cmd('wincmd ' .. direction)
+    local target_win = vim.api.nvim_get_current_win()
+
+    if cur_win == target_win then
+      -- No neighbor in that direction
+      return
+    end
+
+    local target_buf = vim.api.nvim_win_get_buf(target_win)
+
+    -- Swap buffers between current and target window
+    vim.api.nvim_win_set_buf(cur_win, target_buf)
+    vim.api.nvim_win_set_buf(target_win, cur_buf)
+
+    -- Move cursor back to the target window (after swap)
+    vim.api.nvim_set_current_win(target_win)
+  end
+
+  vim.keymap.set('n', '<leader>j', function()
+    swap_buffer_with_neighbor 'h'
+  end, { desc = 'Swap buffer with left' })
+  vim.keymap.set('n', '<leader>k', function()
+    swap_buffer_with_neighbor 'j'
+  end, { desc = 'Swap buffer with down' })
+  vim.keymap.set('n', '<leader>l', function()
+    swap_buffer_with_neighbor 'k'
+  end, { desc = 'Swap buffer with up' })
+  vim.keymap.set('n', '<leader>;', function()
+    swap_buffer_with_neighbor 'l'
+  end, { desc = 'Swap buffer with right' })
 
   map('n', '<A-a>', '<Cmd>vertical resize -2<CR>', { desc = 'Decrease window width', noremap = true })
   map('n', '<A-s>', '<Cmd>horizontal resize -2<CR>', { desc = 'Decrease window height', noremap = true })
