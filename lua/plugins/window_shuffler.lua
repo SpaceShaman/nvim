f2bad2e8c18f7d2e99e60c6668abd9254269b89d
SpaceShaman <spaceshaman@tuta.io>
2025-07-03 18:37:11 +0200

    ♻️ (init.lua): comment out windows-swaper to replace with window-shuffler ✨ (window_shuffler.lua): add new plugin for window management 🔥 (windows-swaper.lua): remove deprecated windows-swaper module

---
+++ lua/plugins/window_shuffler.lua
@@ -0,0 +1,11 @@
+return {
+  'SpaceShaman/window-shuffler.nvim',
+  opts = {
+    keymaps = {
+      left = '<Leader>j',
+      down = '<Leader>k',
+      up = '<Leader>l',
+      right = '<Leader>;',
+    },
+  },
+}
