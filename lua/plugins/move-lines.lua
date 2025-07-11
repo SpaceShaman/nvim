c70850e5d8db73f8679727a65a193df5f5588757
SpaceShaman <spaceshaman@tuta.io>
2025-07-11 14:34:47 +0200

    ✨ (plugins): add new plugins for enhanced editor functionality
    Add 'indent-blankline.nvim' for colorful indentation guides.
    Add 'mini.move' for line and selection movement with key mappings.
    Add 'multicursor.nvim' for multiple cursor support with custom keymaps.
    Add 'window-shuffler.nvim' for window management with key mappings.

---
+++ lua/plugins/move-lines.lua
@@ -0,0 +1,18 @@
+return {
+  'echasnovski/mini.move',
+  version = 'V0.16.0',
+  opts = {
+    mappings = {
+      -- Move visual selection in Visual mode.
+      left = '<A-Left>',
+      right = '<A-Right>',
+      down = '<A-Down>',
+      up = '<A-Up>',
+      -- Move current line in Normal mode
+      line_left = '<A-Left>',
+      line_right = '<A-Right>',
+      line_down = '<A-Down>',
+      line_up = '<A-Up>',
+    },
+  },
+}
