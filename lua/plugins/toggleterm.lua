53975cb6eaf9ca6073026f8f7af22443a66ae82d
SpaceShaman <spaceshaman@tuta.io>
2025-07-11 13:41:51 +0200

    ♻️ (toggleterm.lua): simplify table structure by removing unnecessary nesting

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,13 +1,11 @@
 return {
-  {
-    'akinsho/toggleterm.nvim',
-    version = '*',
-    opts = {
-      open_mapping = 't',
-      autochdir = true,
-      shell = 'fish',
-      terminal_mappings = false,
-      insert_mappings = false,
-    },
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  opts = {
+    open_mapping = 't',
+    autochdir = true,
+    shell = 'fish',
+    terminal_mappings = false,
+    insert_mappings = false,
   },
 }
