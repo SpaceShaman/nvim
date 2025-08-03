7a87340cbb3d912f63a617eccb7f6f7aa613ed80
SpaceShaman <spaceshaman@tuta.io>
2025-08-03 20:36:39 +0200

    ✨ (toggleterm.lua): enhance lazygit terminal with custom float options and key mappings
    Add curved border and transparency to lazygit terminal.
    Implement key mappings for better user experience.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -17,7 +17,23 @@ return {
       insert_mappings = false,
     }
     local Terminal = require('toggleterm.terminal').Terminal
-    local lazygit = Terminal:new { cmd = 'lazygit', hidden = true, direction = 'float', dir = 'git_dir' }
+    local lazygit = Terminal:new {
+      cmd = 'lazygit',
+      dir = 'git_dir',
+      direction = 'float',
+      float_opts = {
+        border = 'curved',
+        winblend = 8,
+      },
+      on_open = function(term)
+        vim.cmd 'startinsert!'
+        vim.keymap.set('t', '<esc>', '<nop>')
+        vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
+      end,
+      on_close = function(term)
+        vim.cmd 'startinsert!'
+      end,
+    }
 
     function _lazygit_toggle()
       lazygit:toggle()
