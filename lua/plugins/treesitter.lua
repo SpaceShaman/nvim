d05fb0cee1d6f9669215993ce76e8ab9b69d5d84
SpaceShaman <spaceshaman@tuta.io>
2025-08-03 13:31:05 +0200

    🔧 (treesitter.lua): add nvim-treesitter-context as a dependency to enhance context awareness in code navigation

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,9 @@ return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   main = 'nvim-treesitter.configs',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-context',
+  },
   opts = {
     ensure_installed = {
       'bash',
@@ -27,6 +30,5 @@ return {
   -- with nvim-treesitter. You should go explore a few and see what interests you:
   --
   --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
   --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 }
