bd0105f0c57d8bacc63681b695683a9f99680f0d
SpaceShaman <spaceshaman@tuta.io>
2025-08-03 20:21:04 +0200

    ✨ (toggleterm.lua): add lazygit integration with toggle function
    Refactor toggleterm configuration to use setup function
    
    Add key mapping for lazygit toggle with <leader>g

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,11 +1,28 @@
 return {
   'akinsho/toggleterm.nvim',
   version = '*',
-  opts = {
-    open_mapping = 't',
-    autochdir = true,
-    shell = 'fish',
-    terminal_mappings = false,
-    insert_mappings = false,
-  },
+  -- opts = {
+  --   open_mapping = 't',
+  --   autochdir = true,
+  --   shell = 'fish',
+  --   terminal_mappings = false,
+  --   insert_mappings = false,
+  -- },
+  config = function()
+    require('toggleterm').setup {
+      open_mapping = 't',
+      autochdir = true,
+      shell = 'fish',
+      terminal_mappings = false,
+      insert_mappings = false,
+    }
+    local Terminal = require('toggleterm.terminal').Terminal
+    local lazygit = Terminal:new { cmd = 'lazygit', hidden = true, direction = 'float', dir = 'git_dir' }
+
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+
+    vim.api.nvim_set_keymap('n', '<leader>g', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
+  end,
 }
