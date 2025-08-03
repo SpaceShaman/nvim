f4dd429968073651463c9bd6a0f9453f7bafddf2
SpaceShaman <spaceshaman@tuta.io>
2025-08-03 13:36:27 +0200

    ♻️ (which-key.lua): clean up comments and improve readability
    Remove redundant comments to streamline the configuration file.

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,12 +1,9 @@
-return { -- Useful plugin to show you pending keybinds.
+return {
   'folke/which-key.nvim',
-  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  event = 'VimEnter',
   opts = {
-    -- delay between pressing a key and opening which-key (milliseconds)
-    -- this setting is independent of vim.o.timeoutlen
     delay = 0,
     icons = {
-      -- set icon mappings to true if you have a Nerd Font
       mappings = vim.g.have_nerd_font,
     },
     -- Document existing key chains
