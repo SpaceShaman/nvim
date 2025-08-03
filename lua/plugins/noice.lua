2179f853d48577b12212d3f925359c34e7079b75
SpaceShaman <spaceshaman@tuta.io>
2025-08-03 12:53:03 +0200

    ✨ (noice.lua): add configuration for noice plugin
    Introduce setup function to configure noice plugin with LSP
    overrides and presets. Add key mappings for message commands
    to enhance user interaction with the plugin.

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -10,5 +10,32 @@ return {
       },
     },
   },
-  opts = {},
+  config = function()
+    require('noice').setup {
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+      presets = {
+        long_message_to_split = true, -- long messages will be sent to a split
+      },
+    }
+    local keymap = vim.keymap.set
+
+    keymap('n', '<leader>ml', function()
+      require('noice').cmd 'last'
+    end, { desc = 'Show last message' })
+
+    keymap('n', '<leader>mh', function()
+      require('noice').cmd 'telescope'
+    end, { desc = 'Show message history' })
+
+    keymap('n', '<leader>mc', function()
+      require('noice').cmd 'dismiss'
+    end, { desc = 'Close messages' })
+  end,
 }
