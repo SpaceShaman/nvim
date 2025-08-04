da162cfb4fedde2bb64b86d9fcc04b4cc94806d8
SpaceShaman <spaceshaman@tuta.io>
2025-08-04 08:37:50 +0200

    🐛 (toggleterm.lua): fix escape key mapping in terminal mode to allow exiting insert mode

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -19,7 +19,7 @@ return {
       },
       on_open = function(term)
         vim.cmd 'startinsert!'
-        vim.api.nvim_buf_set_keymap(term.bufnr, 't', '<esc>', '<nop>', { noremap = true, silent = true })
+        vim.api.nvim_buf_set_keymap(term.bufnr, 't', '<esc>', '<esc>', { noremap = true, silent = true })
         vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', '<cmd>close<CR>', { noremap = true, silent = true })
       end,
       on_close = function(term)
