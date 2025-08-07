dbaa557f93fbde30288c865c648e25dde0257ed1
SpaceShaman <spaceshaman@tuta.io>
2025-08-07 18:17:43 +0200

    ♻️ (neotest.lua): refactor key mappings to use vim.keymap.set for improved readability and maintainability
    📝 (which-key.lua): correct group name from '[T]est' to '[T]ests' for consistency with other key mappings

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -15,75 +15,37 @@ return {
           runner = 'pytest',
         },
       },
-      output = { open_on_run = true },
     }
 
     local neotest = require 'neotest'
-    require('which-key').add {
-      { '<leader>t', group = '[T]ests' },
-      {
-        '<leader>tn',
-        function()
-          neotest.run.run()
-        end,
-        desc = 'Run nearest test',
-      },
-      {
-        '<leader>tf',
-        function()
-          neotest.run.run(vim.fn.expand '%')
-        end,
-        desc = 'Run tests in current file',
-      },
-      {
-        '<leader>ta',
-        function()
-          neotest.run.run { suite = true }
-        end,
-        desc = 'Run all tests',
-      },
-      {
-        '<leader>td',
-        function()
-          neotest.run.run { strategy = 'dap' }
-        end,
-        desc = 'Debug nearest test',
-      },
-      {
-        '<leader>ts',
-        function()
-          neotest.run.stop()
-        end,
-        desc = 'Stop tests',
-      },
-      {
-        '<leader>to',
-        function()
-          neotest.output.open()
-        end,
-        desc = 'Show test output',
-      },
-      {
-        '<leader>tp',
-        function()
-          neotest.output_panel.toggle()
-        end,
-        desc = 'Toggle test output panel',
-      },
-      {
-        '<leader>tt',
-        function()
-          neotest.summary.toggle()
-        end,
-        desc = 'Toggle test summary',
-      },
-      {
-        '<leader>tc',
-        function()
-          neotest.run.run { suite = true, env = { CI = true } }
-        end,
-        desc = 'Run all tests in CI mode',
-      },
-    }
+    local keymap = vim.keymap.set
+
+    keymap('n', '<leader>tn', function()
+      neotest.run.run()
+    end, { desc = 'Run nearest test' })
+    keymap('n', '<leader>tf', function()
+      neotest.run.run(vim.fn.expand '%')
+    end, { desc = 'Run tests in current file' })
+    keymap('n', '<leader>ta', function()
+      neotest.run.run { suite = true }
+    end, { desc = 'Run all tests' })
+    keymap('n', '<leader>td', function()
+      neotest.run.run { strategy = 'dap' }
+    end, { desc = 'Debug nearest test' })
+    keymap('n', '<leader>ts', function()
+      neotest.run.stop()
+    end, { desc = 'Stop tests' })
+    keymap('n', '<leader>to', function()
+      neotest.output.open()
+    end, { desc = 'Show test output' })
+    keymap('n', '<leader>tp', function()
+      neotest.output_panel.toggle()
+    end, { desc = 'Toggle test output panel' })
+    keymap('n', '<leader>tt', function()
+      neotest.summary.toggle()
+    end, { desc = 'Toggle test summary' })
+    keymap('n', '<leader>tc', function()
+      neotest.run.run { suite = true, env = { CI = true } }
+    end, { desc = 'Run all tests in CI mode' })
   end,
 }
