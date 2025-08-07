8468c97af34ca774f1e6207bbce6a49d06fc546c
SpaceShaman <spaceshaman@tuta.io>
2025-08-07 18:03:29 +0200

    ✨ (neotest.lua): add 'which-key.nvim' for enhanced keybinding support ♻️ (neotest.lua): refactor key mappings to use 'which-key' for better UX ✨ (neotest.lua): add new test commands for running all tests and CI mode ♻️ (which-key.lua): update test group keybinding to '<leader>t' for consistency

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -6,34 +6,84 @@ return {
     'antoinemadec/FixCursorHold.nvim',
     'nvim-treesitter/nvim-treesitter',
     'nvim-neotest/neotest-python',
+    'folke/which-key.nvim',
   },
   config = function()
     require('neotest').setup {
       adapters = {
         require 'neotest-python' {
-          dap = { justMyCode = false },
           runner = 'pytest',
         },
       },
+      output = { open_on_run = true },
     }
 
-    local map = vim.keymap.set
     local neotest = require 'neotest'
-
-    map('n', '<leader>tt', function()
-      neotest.summary.toggle()
-    end, { desc = 'Toggle test summary' })
-
-    map('n', '<leader>tn', function()
-      neotest.run.run()
-    end, { desc = 'Run nearest test' })
-
-    map('n', '<leader>tf', function()
-      neotest.run.run(vim.fn.expand '%')
-    end, { desc = 'Run tests in current file' })
-
-    map('n', '<leader>td', function()
-      neotest.run.run { strategy = 'dap' }
-    end, { desc = 'Debug nearest test' })
+    require('which-key').add {
+      { '<leader>t', group = '[T]ests' },
+      {
+        '<leader>tn',
+        function()
+          neotest.run.run()
+        end,
+        desc = 'Run nearest test',
+      },
+      {
+        '<leader>tf',
+        function()
+          neotest.run.run(vim.fn.expand '%')
+        end,
+        desc = 'Run tests in current file',
+      },
+      {
+        '<leader>ta',
+        function()
+          neotest.run.run { suite = true }
+        end,
+        desc = 'Run all tests',
+      },
+      {
+        '<leader>td',
+        function()
+          neotest.run.run { strategy = 'dap' }
+        end,
+        desc = 'Debug nearest test',
+      },
+      {
+        '<leader>ts',
+        function()
+          neotest.run.stop()
+        end,
+        desc = 'Stop tests',
+      },
+      {
+        '<leader>to',
+        function()
+          neotest.output.open()
+        end,
+        desc = 'Show test output',
+      },
+      {
+        '<leader>tp',
+        function()
+          neotest.output_panel.toggle()
+        end,
+        desc = 'Toggle test output panel',
+      },
+      {
+        '<leader>tt',
+        function()
+          neotest.summary.toggle()
+        end,
+        desc = 'Toggle test summary',
+      },
+      {
+        '<leader>tc',
+        function()
+          neotest.run.run { suite = true, env = { CI = true } }
+        end,
+        desc = 'Run all tests in CI mode',
+      },
+    }
   end,
 }
