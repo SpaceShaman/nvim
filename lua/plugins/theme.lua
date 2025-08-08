c406148dbe2c6a61ceebbae6830e73263b508ed0
SpaceShaman <spaceshaman@tuta.io>
2025-08-08 13:58:06 +0200

    ⬆️ (theme.lua): update theme plugin source from 'Shatur/neovim-ayu' to 'SpaceShaman/neovim-ayu' to ensure compatibility and access to latest features

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,5 +1,5 @@
 return {
-  'Shatur/neovim-ayu',
+  'SpaceShaman/neovim-ayu',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
     require('ayu').setup {
