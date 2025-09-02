46a43c1f7d625dda58ccea3607f7fe6754cddaab
SpaceShaman <spaceshaman@tuta.io>
2025-09-02 10:38:55 +0200

    ✨ (autoformat.lua): add 'ruff_format' to python formatters to enhance code formatting options

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -20,7 +20,7 @@ return {
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
-      python = { 'ruff_fix', 'ruff_organize_imports' },
+      python = { 'ruff_fix', 'ruff_organize_imports', 'ruff_format' },
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
     },
