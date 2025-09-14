8942445c78a71a449fef9531843247728f126497
SpaceShaman <spaceshaman@tuta.io>
2025-09-14 19:13:52 +0200

    ♻️ (lsp_servers.lua, lsp.lua): refactor LSP server configuration
    Extract LSP server configurations into a separate file
    `lsp_servers.lua` for better modularity and maintainability.
    This change simplifies `lsp.lua` by removing the inline server
    configurations and requiring them from the new file.

---
+++ lua/lsp_servers.lua
@@ -0,0 +1,51 @@
+local home_dir = vim.fn.expand '~'
+return {
+  lua_ls = {
+    settings = {
+      Lua = {
+        completion = {
+          callSnippet = 'Replace',
+        },
+      },
+    },
+  },
+  ruff = {},
+  pyright = {
+    settings = {
+      pyright = {
+        -- Using Ruff's import organizer
+        disableOrganizeImports = true,
+      },
+      python = {
+        analysis = {
+          -- Ignore all files for analysis to exclusively use Ruff for linting
+          ignore = { '*' },
+          diagnosticMode = 'workspace',
+          useLibraryCodeForTypes = true,
+        },
+      },
+    },
+  },
+  ts_ls = {
+    init_options = {
+      plugins = {
+        {
+          name = '@vue/typescript-plugin',
+          location = vim.fn.expand '$MASON/packages/vue-language-server/node_modules/@vue/language-server',
+          languages = { 'vue' },
+          configNamespace = 'typescript',
+        },
+      },
+    },
+    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+  },
+  tailwindcss = {},
+  stylua = {}, -- Used to format Lua code
+  prettier = {},
+  vue_ls = {},
+  expert = {
+    cmd = { home_dir .. '/.local/bin/expert' },
+    root_markers = { 'mix.exs', '.git' },
+    filetypes = { 'elixir', 'eelixir', 'heex' },
+  },
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -150,59 +150,7 @@ return {
       }
 
       local capabilities = require('blink.cmp').get_lsp_capabilities()
-      local home_dir = vim.fn.expand '~'
-
-      local servers = {
-        lua_ls = {
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-            },
-          },
-        },
-        ruff = {},
-        pyright = {
-          settings = {
-            pyright = {
-              -- Using Ruff's import organizer
-              disableOrganizeImports = true,
-            },
-            python = {
-              analysis = {
-                -- Ignore all files for analysis to exclusively use Ruff for linting
-                ignore = { '*' },
-                diagnosticMode = 'workspace',
-                useLibraryCodeForTypes = true,
-              },
-            },
-          },
-        },
-        ts_ls = {
-          init_options = {
-            plugins = {
-              {
-                name = '@vue/typescript-plugin',
-                location = vim.fn.expand '$MASON/packages/vue-language-server/node_modules/@vue/language-server',
-                languages = { 'vue' },
-                configNamespace = 'typescript',
-              },
-            },
-          },
-          filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
-        },
-        tailwindcss = {},
-        expert = {
-          cmd = { home_dir .. '/.local/bin/expert' },
-          root_markers = { 'mix.exs', '.git' },
-          filetypes = { 'elixir', 'eelixir', 'heex' },
-        },
-        stylua = {}, -- Used to format Lua code
-        prettier = {},
-        vue_ls = {},
-      }
-
+      local servers = require 'lsp_servers'
       require('mason-tool-installer').setup { ensure_installed = vim.tbl_keys(servers) }
 
       for name, cfg in pairs(servers) do
