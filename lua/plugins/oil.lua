91332de9901f70bbcea7df28f5be611be954dbc9
SpaceShaman <spaceshaman@tuta.io>
2025-09-17 11:35:58 +0200

    ✨ (oil.lua): add auto-open feature for oil on startup if no files are open

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,10 +4,27 @@ return {
   ---@type oil.SetupOpts
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   lazy = false,
-  opts = {
-    keymaps = {
-      ['<C-_>'] = { 'actions.show_help', mode = 'n' },
-      ['<C-v>'] = { 'actions.select', opts = { vertical = true } },
-    },
-  },
+  config = function()
+    require('oil').setup {
+      keymaps = {
+        ['<C-_>'] = { 'actions.show_help', mode = 'n' },
+        ['<C-v>'] = { 'actions.select', opts = { vertical = true } },
+      },
+    }
+    -- Open oil on startup if no files are open
+    vim.api.nvim_create_autocmd('VimEnter', {
+      once = true,
+      callback = function()
+        if vim.fn.argc() > 0 then
+          return
+        end
+        vim.schedule(function()
+          local listed = vim.fn.getbufinfo { buflisted = 1 }
+          if #listed == 1 and listed[1].name == '' then
+            require('oil').open()
+          end
+        end)
+      end,
+    })
+  end,
 }
