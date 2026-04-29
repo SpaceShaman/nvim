108377e611c5d21d1f56b56bc9d82fa4689e006d
SpaceShaman <spaceshaman@tuta.io>
2026-04-29 08:29:33 +0200

    ♻️ (marks.lua): refactor mark checking logic into a separate function for clarity ✨ (marks.lua): add notification for existing mark on the current line

--- lua/marks.lua
+++ lua/marks.lua
@@ -68,18 +68,11 @@ local function setup_global_mark_navigation()
 end
 
 local function next_free_mark()
-  local cur_bufnr = vim.fn.bufnr '%'
-  local cur_file = vim.fn.fnamemodify(vim.fn.expand '%:p', ':p')
-  local cur_line = vim.fn.line '.'
   local used = {}
 
   for _, mark in ipairs(vim.fn.getmarklist()) do
     local name = mark.mark:sub(2, 2)
     if name:match '%u' then
-      local same = (mark.pos[1] ~= 0 and mark.pos[1] == cur_bufnr) or (mark.file ~= '' and vim.fn.fnamemodify(mark.file, ':p') == cur_file)
-      if same and mark.pos[2] == cur_line then
-        return nil -- mark already exists on this line
-      end
       used[name] = true
     end
   end
@@ -93,17 +86,38 @@ local function next_free_mark()
   return nil -- all A–Z occupied
 end
 
+local function is_mark_exists_on_line()
+  local cur_bufnr = vim.fn.bufnr '%'
+  local cur_file = vim.fn.fnamemodify(vim.fn.expand '%:p', ':p')
+  local cur_line = vim.fn.line '.'
+
+  for _, mark in ipairs(vim.fn.getmarklist()) do
+    local name = mark.mark:sub(2, 2)
+    if name:match '%u' then
+      local same = (mark.pos[1] ~= 0 and mark.pos[1] == cur_bufnr) or (mark.file ~= '' and vim.fn.fnamemodify(mark.file, ':p') == cur_file)
+      if same and mark.pos[2] == cur_line then
+        return true
+      end
+    end
+  end
+  return false
+end
+
 return function()
   set_workspace_shadafile()
   setup_global_mark_navigation()
 
   vim.keymap.set('n', 'm', function()
     local c = vim.fn.getcharstr()
+    if is_mark_exists_on_line() then
+      vim.notify('Mark already added: ' .. string.upper(c), vim.log.levels.WARN)
+      return
+    end
     if c:match '%l' then
       local mark = next_free_mark()
       if mark then
         vim.cmd('mark ' .. mark)
-        vim.notify('Mark added: ' .. mark, vim.log.levels.INFO)
+        vim.notify('Mark added: ' .. mark, vim.log.levels.WARN)
       else
         vim.notify('No free mark (A–Z) or line already marked', vim.log.levels.WARN)
       end
