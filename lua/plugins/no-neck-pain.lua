5d797f217e2e1484b0e3470f954e4b3e7afe8c90
SpaceShaman <spaceshaman@tuta.io>
2026-06-11 10:31:53 +0200

    🔧 (no-neck-pain.lua): remove neotest integration to prevent conflicts

--- lua/plugins/no-neck-pain.lua
+++ lua/plugins/no-neck-pain.lua
@@ -2,6 +2,8 @@ return {
   'shortcuts/no-neck-pain.nvim',
   commit = '4bc52782524fd50c5658685d584df6fa48fe49f9',
   config = function()
+    require('no-neck-pain.util.constants').INTEGRATIONS.neotest = nil
+
     require('no-neck-pain').setup {
       autocmds = {
         enableOnVimEnter = true,
