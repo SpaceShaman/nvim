c820b8502c9be78475a81d3e1258567379bf9341
SpaceShaman <spaceshaman@tuta.io>
2026-06-21 17:45:23 +0200

    ♻️ (autocompletion.lua): refactor auto_show to only trigger in command mode 💡 (autocompletion.lua): remove extensive comments on keymap presets for clarity

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -35,31 +35,13 @@ return { -- Autocompletion
       keymap = { preset = 'inherit' },
       completion = {
         menu = {
-          auto_show = true,
+          auto_show = function()
+            return vim.fn.getcmdtype() == ':'
+          end,
         },
       },
     },
     keymap = {
-      -- 'default' (recommended) for mappings similar to built-in completions
-      --   <c-y> to accept ([y]es) the completion.
-      --    This will auto-import if your LSP supports it.
-      --    This will expand snippets if the LSP sent a snippet.
-      -- 'super-tab' for tab to accept
-      -- 'enter' for enter to accept
-      -- 'none' for no mappings
-      --
-      -- For an understanding of why the 'default' preset is recommended,
-      -- you will need to read `:help ins-completion`
-      --
-      -- No, but seriously. Please read `:help ins-completion`, it is really good!
-      --
-      -- All presets have the following mappings:
-      -- <tab>/<s-tab>: move to right/left of your snippet expansion
-      -- <c-space>: Open menu or open docs if already open
-      -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-      -- <c-e>: Hide menu
-      -- <c-k>: Toggle signature help
-      --
       -- See :h blink-cmp-config-keymap for defining your own keymap
       preset = 'enter',
       ['<Tab>'] = { 'fallback' },
