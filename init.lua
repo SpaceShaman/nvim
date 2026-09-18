d8ba73237d8e94f7772ef21f3e8fe90336310b5a
SpaceShaman <spaceshaman@tuta.io>
2026-09-18 08:35:51 +0000

    ✨ (init.lua): enhance lazy loading setup by adding performance options for improved runtime efficiency

--- init.lua
+++ init.lua
@@ -118,9 +118,11 @@ end
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
-require('lazy').setup {
+require('lazy').setup({
   { import = 'plugins' },
-}
+}, {
+  performance = { rtp = { reset = false } },
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
